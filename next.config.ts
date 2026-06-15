6a80267ce608fa479358d1a183f7f469885df418
Dimas Angkasa Nurindra <mas.angkasa27@gmail.com>
2026-06-15 13:30:56 +0700

    fix: Change images quality and update image assets

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ export default function createNextConfig(): NextConfig {
   const nextConfig: NextConfig = {
     reactCompiler: true,
     // outputFileTracingRoot: process.cwd(),
+    images: {
+      qualities: [80, 100],
+    },
   };
 
   return nextConfig;
